463c1e4c2eb2b6d5238769cafe9183b506412330
zoe <aipe-tester@google.com>
2025-08-08 13:46:11 +0800

    Modify: backend/resources/trail.py

--- backend/app.py
+++ backend/app.py
@@ -9,8 +9,6 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_restful import Api, Resource
-from resources.gpxanalyzer import GpxAnalyzer
-from resources.map import Map
 from resources.tiles import Tiles
 from resources.trails import Trail, Trails
 from resources.weather import Weather
@@ -27,8 +25,6 @@ api = Api(app)
 api.add_resource(Trails, "/api/trails")
 api.add_resource(Trail, "/api/trails/<string:id>")
 api.add_resource(Weather, "/api/weather/<string:location_name>")
-api.add_resource(GpxAnalyzer, "/api/gpxanalyzer")
-api.add_resource(Map, "/api/map/coordinates")
 api.add_resource(Tiles, "/api/tiles/download")
 
 # --- 啟動伺服器 ---
